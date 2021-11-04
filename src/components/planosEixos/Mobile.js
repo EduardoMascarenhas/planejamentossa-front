105883d478a3cf44712c895caf2840f4c3894c38
EduardoMascarenhas <eduardomsmascarenhas@gmail.com>
2021-11-04 16:44:16 -0300

    cores custom

--- src/components/planosEixos/Mobile.js
+++ src/components/planosEixos/Mobile.js
@@ -75,10 +75,14 @@ const PEMobile = () => {
                                     src={`${API}/eixo/thumb/${e.slug}`}
                                     alt=""
                                     class="thumbnail rounded-circle"
+                                    style={{ background: `${e.borderColor}` }}
                                   />
                                 </div>
                               </div>
-                              <div class="text-white list-title">
+                              <div
+                                class="text-white list-title"
+                                style={{ border: `2px solid ${e.borderColor}` }}
+                              >
                                 <div class="name text-uppercase">{e.title}</div>
                               </div>
                             </grid-item>
